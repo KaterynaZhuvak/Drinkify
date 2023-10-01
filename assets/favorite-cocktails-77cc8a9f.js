850e78e3d2a6b4fc8acfa3114753dccfb5d4111a
KulibabaYuliia <129789806+KulibabaYuliia@users.noreply.github.com>
2023-10-01 19:58:52 +0000

    Deploying to gh-pages from @ KaterynaZhuvak/Drinkify@dd3d62c4a0241f957c39b0ec807d28de72884dfc 🚀

---
+++ assets/favorite-cocktails-77cc8a9f.js
@@ -0,0 +1,1 @@
+import"./header_backdrop-606ecd43.js";const o=document.querySelector(".cardlist");console.log(o);const c=document.querySelector(".plug");console.log(c);
--- assets/favorite-cocktails-b7f0b073.js
+++
@@ -1,1 +0,0 @@
-import"./styles-15d108fa.js";const o=document.querySelector(".cardlist");console.log(o);const c=document.querySelector(".plug");console.log(c);
