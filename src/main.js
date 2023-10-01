88f3b339f752e73a06867457f937db96715e50bf
KaterynaZhuvak <129985868+KaterynaZhuvak@users.noreply.github.com>
2023-10-01 06:32:20 -0400

    updated

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import './js/keyboard';
 import './js/alphabetMenu';
 import './js/pagination';
 import './js/search';
-import './js/favorite-ingredients';
+// import './js/favorite-ingredients';
 import './js/popupcocktails';
 import './js/scroll-up';
 import './js/footer';
