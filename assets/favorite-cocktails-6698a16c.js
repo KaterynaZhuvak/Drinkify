1d3e501b9bd7dfeb0d69f096ea510cd7b0942282
KaterynaZhuvak <129985868+KaterynaZhuvak@users.noreply.github.com>
2023-10-01 01:06:29 +0000

    Deploying to gh-pages from @ KaterynaZhuvak/Drinkify@7405660f9483abbcc29ea8a7816902527405b4e6 🚀

---
+++ assets/favorite-cocktails-6698a16c.js
@@ -0,0 +1,1 @@
+import"./styles-5f1d216f.js";const o=document.querySelector(".cardlist");console.log(o);const c=document.querySelector(".plug");console.log(c);
--- assets/favorite-cocktails-887d92c6.js
+++
@@ -1,1 +0,0 @@
-import"./styles-32c47b58.js";const o=document.querySelector(".cardlist");console.log(o);const c=document.querySelector(".plug");console.log(c);
