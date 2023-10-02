b5215fbb61636da667bdd66390c8b6060d92dd65
AndriiSkliar <89527037+AndriiSkliar@users.noreply.github.com>
2023-10-02 09:49:33 +0000

    Deploying to gh-pages from @ KaterynaZhuvak/Drinkify@8cfe8a7e9acfb219390a979b2dc201ec9700e022 🚀

--- assets/favorite-cocktails-2bb0c881.js
+++
@@ -1,9 +0,0 @@
-import"./basicLightbox.min-49a1ec45.js";import{f as i,N as e}from"./popupcocktails-79479536.js";const a=document.querySelector(".cardlist"),o=document.querySelector(".plug"),l=document.querySelector(".container-section"),n=document.querySelector(".section-tittle"),d="r",u="cocktails/";let m=6;i(u,d,m).then(t=>{const s=t.map(c=>h(c)).join("");o.classList.add("display"),l.classList.add("container-section-with-cards"),n.classList.add("section-tittle-with-cards"),a.innerHTML=s}).catch(t=>{e.Notify.failure("Server error"),console.log(t)});function h({drinkThumb:t,drink:s,description:c,_id:r}){return`<li class="cardlist-item" data-id=${r}>
-        <img src="${t}" class="cardlist-img" alt="${s}" onerror="this.onerror=null;this.src='img/rafiki.jpg';" width=300>
-        <h3 class="cardlist-drink">${s}</h3>
-        <p class="cardlist-descr">${c}</p>
-        <div class="cartlist-btns"><button class="cardlist-learn">learn more</button><button class="cardlist-fav"><svg
-                class="cardlist-svg" weight="18" height="18">
-                <use href="img/sprite.svg#trash"></use>
-            </svg></button></div>
-        </li>`}
---
+++ assets/favorite-ingredients-cee8594f.js
@@ -0,0 +1,31 @@
+import{b as d}from"./basicLightbox.min-a67160ad.js";const c=document.querySelector(".favorite-ingredients-list"),l=document.querySelector(".sorry-ingredients"),i=JSON.parse(localStorage.getItem("KEY_FAVORITE_INGREDIENTS"))??[];i.splice(0,6);l.classList.add("hidden");r(i,c);i.length||l.classList.remove("hidden");c.addEventListener("click",g);function r(n,e){const s=n.map(t=>{let o="Alcoholic";return t.alcohol==="No"&&(o="Non-alcoholic"),`<li class="in-card" data-id=${t._id}>
+        <h3 class="in-card-title">${t.title}</h3>
+        <p class="in-card-alco">${o}</p>
+        <p class="in-card-descr">${t.description||"No data"}</p>
+        <div class="in-card-btns"><button class="btn-learn-more">learn more</button><button class="btn-remove"><svg class="remove-icon">
+                        <use href="./img/sprite.svg#trash"></use>
+                    </svg></button></div>
+</li>`}).join("");e.innerHTML=s}function g(n){if(n.target.classList.contains("btn-learn-more")){const e=a(n.target);d.create(`<div id="modal-ingredients" class="modal-in">
+      <button type="button" class="modal-in-close-button close-cocktail-modal-x">
+        <svg class="icon-in-close" width="11" height="11">
+          <use href="./img/sprite.svg#cross"></use>
+        </svg>
+      </button>
+    <div class="descripe-ingredients" data-id="${e._id}"><div class="header-in">
+          <h2 id="ingredients-title" class="ingredients-title">${e.title}</h2>
+          <p class="kind-in">${e.type}</p>
+        </div>
+        <div class="ingredients-information">
+          <p class="main-description-in">${e.description||"No data"}</p>
+          <ul class="ingredients-spec">
+            <li class="ingredients-description">Type: ${e.type||"No data"}</li>
+            <li class="ingredients-description">Country of origin: ${e.country||"No data"}</li>
+            <li class="ingredients-description">Alcohol by volume: ${e.abv||"No data"}</li>
+            <li class="ingredients-description">Flavour: ${e.flavour||"No data"}</li>
+          </ul>
+        </div>
+        <div class="buttons-in">
+          <button class="btn-in remove-btn">REMOVE FROM FAVORITE</button>
+          <button type="button" id="btn-back" class="btn-in btn-back close-cocktail-modal-back">
+            BACK
+          </button></div></div>`,{onShow:t=>{t.element().querySelector(".close-cocktail-modal-back").onclick=t.close,t.element().querySelector(".close-cocktail-modal-x").onclick=t.close,t.element().querySelector(".remove-btn").addEventListener("click",u),t.element().querySelector(".remove-btn").onclick=t.close}}).show()}(n.target.closest(".btn-remove")||n.target.closest(".btn-in"))&&(m(n),i.length||l.classList.remove("hidden"))}function a(n){const e=n.closest(".in-card").dataset.id;return i.find(({_id:s})=>s===e)}function m(n){const e=a(n.target),s=i.findIndex(({_id:t})=>t===e._id);i.splice(s,1),localStorage.setItem("KEY_FAVORITE_INGREDIENTS",JSON.stringify(i)),r(i,c)}function u(n){const e=n.target.closest(".descripe-ingredients").dataset.id,s=i.find(({_id:o})=>o===e),t=i.findIndex(({_id:o})=>o===s._id);i.splice(t,1),localStorage.setItem("KEY_FAVORITE_INGREDIENTS",JSON.stringify(i)),r(i,c)}
