d279cb3c9df8eed54bc7df42a643b27406ebbe92
Андрей Скляр <andreas.skliar@gmail.com>
2023-10-06 13:08:10 +0300

    пофиксил баг основного скролла и анимации

--- src/js/scroll-up.js
+++ src/js/scroll-up.js
@@ -1,14 +1,15 @@
 function scrollToTop() {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        }
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
 
-        window.addEventListener('scroll', function () {
-            const scrollUpButton = document.getElementById('scrollUpButton');
-            if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-                scrollUpButton.style.display = 'block';
-            } else {
-                scrollUpButton.style.display = 'none';
-            }
-        });
+window.addEventListener('scroll', function () {
+  const scrollUpButton = document.getElementById('scrollUpButton');
 
-        document.getElementById('scrollUpButton').addEventListener('click', scrollToTop);
+  if (document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight) {
+    scrollUpButton.style.display = 'flex';
+  } else {
+    scrollUpButton.style.display = 'none';
+  }
+});
+
+document.getElementById('scrollUpButton').addEventListener('click', scrollToTop);
