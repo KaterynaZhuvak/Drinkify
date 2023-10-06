2c843a41115a827b84c78c635972d4d1b20c8cd7
Андрей Скляр <andreas.skliar@gmail.com>
2023-10-06 10:54:11 +0300

    пофиксил скролл

--- src/js/header_burger.js
+++ src/js/header_burger.js
@@ -5,8 +5,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const btnMenuBurger = document.querySelector(".js-header-menu-burger");
   const modalBurger = document.querySelector(".js-header-backdrop");
   const closeModalBurger = document.querySelector(".js-header-menu-close");
+  const body = document.body;
 
   function onClickBurger() {
+    body.style.overflow = "hidden";
     modalBurger.style.transform = "translateX(0)";
     closeModalBurger.addEventListener("click", onClickCloseModalBurger);
     btnMenuBurger.removeEventListener("click", onClickBurger);
@@ -16,6 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const modalBurger = document.querySelector(".js-header-backdrop");
     modalBurger.style.transform = "translateX(100%)";
     btnMenuBurger.addEventListener("click", onClickBurger);
+    body.style.overflow = null;
   }
 
   btnMenuBurger.addEventListener("click", onClickBurger);
